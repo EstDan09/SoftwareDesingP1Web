2e1fc7e6c12d49a9f0c479ef6402b79bafeb8bc8
Esteban Daniel Secaida Vargas <esecaidavargas@gmail.com>
2024-12-29 09:46:19 -0600

    falta otra lista y quitar de fav

--- src/app/pais/components/results/results.component.ts
+++ src/app/pais/components/results/results.component.ts
@@ -1,5 +1,6 @@
 import { Component, input, Input } from '@angular/core';
 import { Country } from '../../models/country.interface';
+import { FireBaseCnxService } from '../../services/firebasecnx.service';
 @Component({
   selector: 'app-results',
   standalone: false,
@@ -11,5 +12,11 @@ export class ResultsComponent {
  
   @Input()
   public countries: Country[] = []; 
+
+  constructor( private _firebase: FireBaseCnxService){}
+
+  addToFavorites(term: string) {
+    this._firebase.saveCountries(term);
+  }
 
 }
